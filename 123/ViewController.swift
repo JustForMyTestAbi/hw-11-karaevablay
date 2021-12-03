c5eb7bcbce054aeffc868a48df1dfbe9921837b5
Gray <>
2021-12-03 14:03:09 +0600

    Initial Commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by ablai erzhanov on 03.12.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("dssfdsfgdsgfdsgdfg")
+        view.window?.backgroundColor = .white
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
